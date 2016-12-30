73244020f3f1dad6dcad30a6afb1276fdb2ece01
Jeff Youngstrom <jeffy@tomecat.com>
2016-12-30 11:34:23 -0800

    req 3: add todos
    Also refactor our variable name

--- program.js
+++ program.js
@@ -1,4 +1,7 @@
 // req 1: store todos
-var todo = ['item 1', 'item 2', 'item 3']
-// req 1: display todos
-console.log("My Todos:", todo)
+var todos = ['item 1', 'item 2', 'item 3']
+// req 2: display todos
+console.log("My Todos:", todos)
+// req 3: add todos
+todos.push('item 4')
+console.log("My Todos:", todos)
