0148f73e84c7f1fd88a1a82d3c4caf550a5bd17e
Jeff Youngstrom <jeffy@tomecat.com>
2016-12-30 11:45:49 -0800

    req 4: change a todo
    and add some minimal style

--- program.js
+++ program.js
@@ -5,3 +5,7 @@ console.log("My Todos:", todos)
 // req 3: add todos
 todos.push('item 4')
 console.log("My Todos:", todos)
+// req 4: change a todo
+todos[1] = 'item 2 v2'
+console.log("My Todos:", todos)
+
