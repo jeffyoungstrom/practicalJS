2bfc119ec1e58600bbb65de3c90c6f0d13c431eb
Jeff Youngstrom <jeffy@tomecat.com>
2016-12-30 12:25:53 -0800

    req 2: function for add
    And change requirement comment numbering.

--- program.js
+++ program.js
@@ -1,18 +1,23 @@
-// req 1: store todos
+// req 1.1: store todos
 var todos = ['item 1', 'item 2', 'item 3']
-// req 2: display todos
+// req 1.2: display todos
 displayTodos();
-// req 3: add todos
-todos.push('item 4')
-displayTodos();
-// req 4: change a todo
+// req 1.3: add todos
+addTodo('item 4');
+// req 1.4: change a todo
 todos[1] = 'item 2 v2'
 displayTodos();
-// req 5: delete a todo
+// req 1.5: delete a todo
 todos.splice(1, 1)
 displayTodos();
 
+// req 2.1: function for display
 function displayTodos() {
     console.log('My Todos:', todos);
 }
 
+// req 2.2: function for adding
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodos();
+}
